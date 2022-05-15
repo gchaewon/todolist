1e165a5685f83b8c5d163fee63720fef8c5b197e
chaewon <89545462+gchaewon@users.noreply.github.com>
2022-05-15 18:04:19 +0900

    second commit

--- todolist.js
+++ todolist.js
@@ -1,14 +1,52 @@
-const init = () => {
-    form.addEventListener("submit", addTodoItem);
+const checkBox = document.querySelector(".check-box");
+checkBox.addEventListener("click",todoCheck);
+//check-box 누르면 check-icon을 추가하고, 글자 색 변경하는 todoCheck 함수
+function todoCheck(){
+    //check-box 누르면 check-icon 추가
+    const check = document.querySelector(".check-icon");
+    check.style.display == "block"? check.style.display = "none": check.style.display = "block"
+    check.addEventListener("click",todoCheck);
+    //check-box 누르면 글자색 변경
+    // const newTodoText = 
 };
-const form = document.querySelector(".todo-form");
-const displayForm = ()=>{ 
-    if (form.style.display == "block")
-        form.style.display = "none";
-    else form.style.display = "block";
+
+const plusTodo = document.querySelector(".plus-button");
+plusTodo.addEventListener("click",addTodoItem);
+
+//plus-button 누르면 todo-form 추가하고, 버튼을 아래로 옮기는 addTodoItem 함수
+function addTodoForm(){
 };
-const button = document.querySelector(".popup-button");
-button.addEventListener("click", displayForm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var now = new Date();
+// var month = now.getMonth();
+// $(".month").text(month+1);
+// var date = now.getDate()+1;
+// const nowdate = document.querySelector("day");
+
+// // $(".day").text(date+1);
+
+// var day = now.getDay();
+// $(".today").text(day);
+
+
+
+
 
 
 
